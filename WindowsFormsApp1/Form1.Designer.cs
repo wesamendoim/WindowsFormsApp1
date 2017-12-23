14c2828c1bddfec8377ae3a10fe42bb2853fe043
wesleyvbnardino <wesleyvbnardino@hotmail.com>
2017-12-22 23:31:51 -0200

    #FORM_LOGIN

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtLogin = new System.Windows.Forms.TextBox();
+            this.TxtSenha = new System.Windows.Forms.TextBox();
+            this.TxtHostName = new System.Windows.Forms.TextBox();
+            this.BtnSalvar = new System.Windows.Forms.Button();
+            this.BtnCancelar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TxtLogin
+            // 
+            this.TxtLogin.Location = new System.Drawing.Point(85, 61);
+            this.TxtLogin.Name = "TxtLogin";
+            this.TxtLogin.Size = new System.Drawing.Size(123, 20);
+            this.TxtLogin.TabIndex = 0;
+            // 
+            // TxtSenha
+            // 
+            this.TxtSenha.Location = new System.Drawing.Point(85, 110);
+            this.TxtSenha.Name = "TxtSenha";
+            this.TxtSenha.PasswordChar = '*';
+            this.TxtSenha.Size = new System.Drawing.Size(123, 20);
+            this.TxtSenha.TabIndex = 1;
+            // 
+            // TxtHostName
+            // 
+            this.TxtHostName.Location = new System.Drawing.Point(85, 15);
+            this.TxtHostName.Name = "TxtHostName";
+            this.TxtHostName.Size = new System.Drawing.Size(380, 20);
+            this.TxtHostName.TabIndex = 2;
+            // 
+            // BtnSalvar
+            // 
+            this.BtnSalvar.Location = new System.Drawing.Point(85, 190);
+            this.BtnSalvar.Name = "BtnSalvar";
+            this.BtnSalvar.Size = new System.Drawing.Size(75, 23);
+            this.BtnSalvar.TabIndex = 3;
+            this.BtnSalvar.Text = "Salvar";
+            this.BtnSalvar.UseVisualStyleBackColor = true;
+            this.BtnSalvar.Click += new System.EventHandler(this.BtnSalvar_Click);
+            // 
+            // BtnCancelar
+            // 
+            this.BtnCancelar.Location = new System.Drawing.Point(390, 190);
+            this.BtnCancelar.Name = "BtnCancelar";
+            this.BtnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.BtnCancelar.TabIndex = 4;
+            this.BtnCancelar.Text = "Cancelar";
+            this.BtnCancelar.UseVisualStyleBackColor = true;
+            this.BtnCancelar.Click += new System.EventHandler(this.BtnCancelar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "HostName";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Login";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Senha";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 239);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnCancelar);
+            this.Controls.Add(this.BtnSalvar);
+            this.Controls.Add(this.TxtHostName);
+            this.Controls.Add(this.TxtSenha);
+            this.Controls.Add(this.TxtLogin);
+            this.Name = "Form1";
+            this.Text = "Frm_Login";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtLogin;
+        private System.Windows.Forms.TextBox TxtSenha;
+        private System.Windows.Forms.TextBox TxtHostName;
+        private System.Windows.Forms.Button BtnSalvar;
+        private System.Windows.Forms.Button BtnCancelar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        XmlDocument DocXml = new XmlDocument();
+
+        private string filePathXml;
+
+        private string fileXml;
+
+        private bool checkFileXml;
+
+        public Form1()
+        {
+            fileXml = InfoGlobal.filePathProject + @"\DocXml.xml";
+
+            checkFileXml = File.Exists(fileXml);
+
+            if (checkFileXml == false)
+            {
+                OpenFileDialog fileDialog = new OpenFileDialog();
+
+                if (fileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filename = fileDialog.FileName;
+
+                    try
+                    {
+                        DocXml.Load(filename);
+
+                        File.Copy(filename, fileXml);
+                    }
+
+                    catch (Exception e)
+                    {
+                        MessageBox.Show("Arquivo incorreto, selecione o xml correto");
+
+
+                    }
+
+                }
+
+
+            }
+
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            //XmlNodeList xmlList;
+
+            TxtHostName.Text = InfoGlobal.HostName; //HostName
+            TxtSenha.Text = InfoGlobal.Password; //Password
+            TxtLogin.Text = InfoGlobal.Login; //Login
+
+            //string password = DocXml.ChildNodes[0].ChildNodes[0].InnerText; //Login
+            //string hostname = DocXml.ChildNodes[0].ChildNodes[1].InnerText; //Password
+            //string login = DocXml.ChildNodes[0].ChildNodes[2].InnerText; //HostName
+
+            //string passwordecrypt = Encryption.Decrypt(password, @"Valadao");
+            //string logindecrypt = Encryption.Decrypt(login, @"Valadao");
+            //string hostnamedecrypt = Encryption.Decrypt(hostname, @"Valadao");
+
+        }
+
+        private void BtnSalvar_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Você deseja salvar as informações?", "Confirmation", MessageBoxButtons.YesNo);
+
+            if (result == DialogResult.Yes)
+            {
+                DocXml.ChildNodes[1].ChildNodes[0].InnerText = TxtLogin.Text; //Login
+                DocXml.ChildNodes[1].ChildNodes[1].InnerText = TxtSenha.Text; //Password
+                DocXml.ChildNodes[1].ChildNodes[2].InnerText = TxtHostName.Text; //HostName
+
+                //Criptografar os dados do formulario
+                string passwordencrypt = Encryption.Encrypt(TxtSenha.Text, @"Valadao");
+                string loginencrypt = Encryption.Encrypt(TxtLogin.Text, @"Valadao");
+                string hostnamencrypt = Encryption.Encrypt(TxtHostName.Text, @"Valadao");
+
+                //Salvar no xml o dado criptografado
+                DocXml.ChildNodes[1].ChildNodes[0].InnerText = loginencrypt;
+                DocXml.ChildNodes[1].ChildNodes[1].InnerText = passwordencrypt;
+                DocXml.ChildNodes[1].ChildNodes[2].InnerText = hostnamencrypt;
+
+                //Salvando o arquivo
+                DocXml.Save(@"C:\Users\Wesley\Desktop\DocXml.xml");
+            }
+        }
+
+        private void BtnCancelar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+
+}
+
+
